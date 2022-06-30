4212e8a22838a9502ce797a95ecd8ffffdfbc7e9
Guy Farley <guy.e.farley@gmail.com>
2022-06-29 17:53:55 -0700

    Additional updates to clients

--- queue-clients/driver.js
+++ queue-clients/driver.js
@@ -7,14 +7,14 @@ driver.subscribe('PICKUP', (payload) => {
   console.log(`Received new order from ${payload.order.store}: ${payload.order.orderId}`);
 
   setTimeout(() => {
-    console.log(`DRIVER: picked up order ${payload.order.orderId}`);
+    // console.log(`DRIVER: picked up order ${payload.order.orderId}`);
     driver.publish('IN-TRANSIT', payload);
-  }, 1000);
+  }, 4000);
 
   setTimeout(() => {
-    console.log(`DRIVER: delivered order ${payload.order.orderId}`);
+    // console.log(`DRIVER: delivered order ${payload.order.orderId}`);
     driver.publish('DELIVERED', payload);
-  }, 2000);
+  }, 6000);
 
   driver.publish('RECEIVED', payload);
 });
---
+++ queue-clients/flower-vendor.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const MessageClient = require('./lib/messageClient');
+const Chance = require('chance');
+const chance = new Chance();
+const vendor = new MessageClient('flower vendor messages');
+
+setInterval(() => {
+  let order = {
+    store: '1-800-flowers',
+    orderId: chance.guid(),
+    customer: chance.name(),
+    address: `${chance.city()}, ${chance.state()}`,
+  };
+
+  console.log('New order sent from', order.store);
+  vendor.publish('PICKUP', { messageId: chance.guid(), order });
+}, 3000);
+
+vendor.subscribe('RECEIVED', (payload) => {
+  console.log(`Confirmed ${payload.orderId} received`);
+});
